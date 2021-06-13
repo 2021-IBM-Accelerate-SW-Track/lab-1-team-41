a058e79e8b41d68178951bc9e32ccc82f0f0a471
nehagb99 <neha.gb99@gmail.com>
2021-06-13 02:32:58 -0400

    Created Basic List Item Functionality

---
+++ src/component/body/list.js
@@ -0,0 +1,41 @@
+import React, { useState }from 'react';
+import ListItem from "./listItem"
+import { Button } from '@material-ui/core';
+import { TextField } from '@material-ui/core';
+
+function List() {
+
+    const [todolist, setTodolist] = useState([]);
+    const [inputTitle, setValue] = useState('');
+
+    let listItems = todolist.map( (todoitem) =>
+        <ListItem title={todoitem.title} dateAdded={todoitem.dateAdded} timeAdded={todoitem.timeAdded} dateDue={todoitem.dateDue}/>
+    )
+
+    function addItem() {
+        let templist = [...todolist];
+        templist.push({
+            title: inputTitle,
+            dateAdded: "",
+            timeAdded: "",
+            dateDue: "",
+        })
+        //date and time variables can be changed from strings by whoever is doing that issue
+        setTodolist(templist);
+        setValue("");
+    }
+
+    return (
+        <div>
+            {listItems}
+            <TextField id="filled-basic" label="Add Item" variant="filled" data-testid="new-item-input"
+                value={inputTitle} 
+			    onChange={(e) => setValue(e.target.value)}/>
+            <Button variant="contained" color="primary" onClick={addItem} data-testid="new-item-button">
+                Add Item
+            </Button>
+        </div>
+    );
+}
+
+export default List;
---
+++ src/component/body/listItem.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ListItem(props) {
+    return (
+        <div>
+            <h1>{props.title}</h1>
+        </div>
+    );
+}
+
+export default ListItem;
