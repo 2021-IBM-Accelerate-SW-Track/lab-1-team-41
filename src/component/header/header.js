dac85b159d692ddd4bdcb9984135de04a8af3e67
Estevan Feliz <felize@bc.edu>
2021-06-14 13:29:24 -0400

    MUI: - added AppBar + Toolbar to the header - added editButton to the appbar

--- src/component/header/header.js
+++ src/component/header/header.js
@@ -1,3 +1,23 @@
+import { AppBar, Toolbar, Typography, Button} from '@material-ui/core';
+import EditIcon from '@material-ui/icons/Edit';
+
 export default function Header(){
-    return <div>2021 IBM Accelerate Software Track: Group 41 To-Do List Application</div>
+    return (
+        <div>
+            <AppBar position='relative' style={{ marginBottom: '1rem'}}>
+                <Toolbar style={{justifyContent: 'space-between'}}>
+                    <Typography variant="h6" style={{justifySelf: 'flex-start'}}>
+                        2021 IBM Accelerate Software Track: Group 41 To-Do List Application
+                    </Typography>
+                    <Button 
+                        color='inherit'
+                        startIcon={<EditIcon/>}
+                        style={{justifySelf:'flex-end'}}
+                    >
+                        Edit
+                    </Button>
+                </Toolbar>
+            </AppBar>
+        </div>
+    );
 }
