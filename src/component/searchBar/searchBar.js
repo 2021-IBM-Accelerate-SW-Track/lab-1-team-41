ff5c7d383fdd5154d9b4c8aabf9e61bd2e427645
Daylin Hart <dhart5@students.asurams.edu>
2021-07-02 15:54:23 -0400

    created search bar feature

--- src/component/searchBar/searchBar.js
+++ src/component/searchBar/searchBar.js
@@ -1,17 +1,17 @@
 import React, {useState} from 'react';
 import SearchBar from "material-ui-search-bar";
-
+ 
 function searchBar(props){
-    return(
-        <div>
-    <SearchBar
-      SearchBar={() => console.log('SearchBar')}
-      onRequestSearch={() => console.log('onRequestSearch')}
-      style={{
-        margin: '0 auto',
-        maxWidth: 900
-      }}
-    />
-        </div>
-    );
+   return(
+       <div>
+   <SearchBar
+     SearchBar={() => console.log('SearchBar')}
+     onRequestSearch={() => console.log('onRequestSearch')}
+     style={{
+       margin: '0 auto',
+       maxWidth: 900
+     }}
+   />
+       </div>
+   );
 }
